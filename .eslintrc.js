752d54aca49bcd052ea38773d39b26162db4ced5
Vitaly <nidastatachna@gmail.com>
2020-06-22 19:15:46 +0300

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2020": true
+    },
+    "extends": ['airbnb-base', 'prettier'],
+    "parser": 'babel-eslint',
+    "rules": {
+        'no-console': 0,
+        'import/prefer-default-export': 0,
+        'prefer-template': 0,
+    },
+};
