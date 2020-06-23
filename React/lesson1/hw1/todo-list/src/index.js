b4971c31f953a663121e16b9b22e81ece436ef1d
Vitaly <nidastatachna@gmail.com>
2020-06-23 22:26:36 +0300

    js

--- React/lesson1/hw1/todo-list/src/index.js
+++ React/lesson1/hw1/todo-list/src/index.js
@@ -6,7 +6,16 @@ import "./index.css"
 const rootElem = document.querySelector('#root')
 
 const elem = (
-    <h1 class="title">Todo List</h1>
+    <>
+        <h1 className="title">Todo List</h1>
+        <main className="todo-list">
+            <div className="actions">
+                <input className="task-input" type="text"></input>
+                <button className="btn create-task-btn">Create</button>
+            </div>
+            <ul className="list"></ul>
+        </main>
+    </>
 );
 
 ReactDOM.render(elem, rootElem)
