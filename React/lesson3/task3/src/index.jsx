1059c6c1e23b84b2c35874bd7573d723a6fa1d7e
Vitaly <nidastatachna@gmail.com>
2020-06-24 21:39:21 +0300

    Components

---
+++ React/lesson3/task3/src/index.jsx
@@ -0,0 +1,19 @@
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Comment from './Comment.jsx'
+import './styles.css'
+
+const rootElem = document.querySelector('#root')
+
+const userInfo = {
+    name: 'Tom',
+    avatarUrl: 'https://www.volynpost.com/img/modules/news/7/6c/aff84ee884aa9094f27910b5582da6c7/top-photo.jpg'
+}
+
+ReactDOM.render(
+    <Comment
+        author={userInfo}
+        text="Good job!"
+        date={new Date('2019-01-01T11:32:19.566Z')}
+    />, rootElem)
