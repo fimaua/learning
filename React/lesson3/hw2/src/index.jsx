e22ff8b92a32c4b2de3d246f84ccdc9d00373090
Vitaly <nidastatachna@gmail.com>
2020-06-25 18:23:13 +0300

    commit

---
+++ React/lesson3/hw2/src/index.jsx
@@ -0,0 +1,22 @@
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Profile from './Profile.jsx'
+import './index.scss'
+
+const rootElem = document.querySelector('#root')
+
+const userData = {
+    firstName: 'Vitaliy',
+    lastName: 'Efimenko',
+    birthDate: new Date('1993-05-19T11:32:19.566Z'),
+    birthPlace: 'Ukraine'
+}
+
+ReactDOM.render(
+    <Profile
+        firstName={userData.firstName}
+        lastName={userData.lastName}
+        birthDate={userData.birthDate}
+        birthPlace={userData.birthPlace}
+    />, rootElem)
