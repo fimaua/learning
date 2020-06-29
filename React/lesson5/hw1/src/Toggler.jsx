882a6f5864901f54dbef480349066b013c460518
Vitaly <nidastatachna@gmail.com>
2020-06-29 19:49:33 +0300

    components

--- React/lesson5/hw1/src/Toggler.jsx
+++ React/lesson5/hw1/src/Toggler.jsx
@@ -4,8 +4,6 @@ class Toggler extends React.Component {
     constructor(props) {
         super(props);
         this.state = { isToggleOn: false };
-
-        // This binding is necessary to make `this` work in the callback
         this.handleClick = this.handleClick.bind(this);
     }
 
