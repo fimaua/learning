2e7bc94b5bf834886f13cadff408e9704ef85b9f
Vitaly <nidastatachna@gmail.com>
2020-07-01 16:36:56 +0300

    components

---
+++ React/lesson6/task3/src/index.jsx
@@ -0,0 +1,10 @@
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Toggler from './Toggler.jsx'
+import './index.scss'
+
+const rootElem = document.querySelector('#root')
+
+ReactDOM.render(
+    <Toggler />, rootElem)
