4b1dbe104f30b3900fb01391d8c200d32a643eef
Vitaly <nidastatachna@gmail.com>
2020-07-01 19:39:49 +0300

    components

--- React/lesson6/hw1/src/Online.jsx
+++ React/lesson6/hw1/src/Online.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const Online = () => {
 
     return (
-        <span className="status__text">All good!</span>
+        <>
+            <span className="status__text">Online</span>
+            <span>All good!</span>
+        </>
     )
 
 }
