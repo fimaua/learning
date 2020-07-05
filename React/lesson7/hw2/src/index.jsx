0645f42827ed9246e0d0377d29c9d8c89d163a2d
Vitaly <nidastatachna@gmail.com>
2020-07-05 22:08:11 +0300

    components

--- React/lesson7/hw2/src/index.jsx
+++ React/lesson7/hw2/src/index.jsx
@@ -1,10 +1,33 @@
 // index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Status from './Status.jsx'
+import UsersList from './UsersList.jsx'
 import './index.scss'
 
 const rootElem = document.querySelector('#root')
 
+const users = [
+    {
+        id: 'id-0',
+        age: 21,
+        name: 'Bob',
+    },
+    {
+        id: 'id-1',
+        age: 17,
+        name: 'Tom',
+    },
+    {
+        id: 'id-2',
+        age: 18,
+        name: 'Tad',
+    },
+    {
+        id: 'id-3',
+        age: 45,
+        name: 'Sam',
+    },
+];
+
 ReactDOM.render(
-    <Status isOnline={true} />, rootElem)
+    <UsersList users={users} />, rootElem)
