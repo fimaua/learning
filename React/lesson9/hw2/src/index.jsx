f0a902afcd91494048608b61d3215dba807b4558
Vitaly <nidastatachna@gmail.com>
2020-07-09 12:31:15 +0300

    game

--- React/lesson9/hw2/src/index.jsx
+++
@@ -1,9 +0,0 @@
-// index.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ConnectionStatus from './ConnectionStatus.jsx'
-import './index.scss'
-
-const rootElem = document.querySelector('#root')
-
-ReactDOM.render(<ConnectionStatus />, rootElem)
