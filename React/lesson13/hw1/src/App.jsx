573cf8dea32dbe4bdc05d02b57063c0c046cc180
Vitaly <nidastatachna@gmail.com>
2020-07-16 19:58:42 +0300

    component

---
+++ React/lesson13/hw1/src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+import Home from "./Home.jsx"
+import Products from "./Products.jsx"
+import Contacts from "./Contacts.jsx"
+import PageNotFound from "./PageNotFound.jsx"
+
+const App = () => {
+    return (
+        <div className="page">
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/products">
+                        <Products />
+                    </Route>
+                    <Route path="/contacts">
+                        <Contacts />
+                    </Route>
+                    <Route path="*">
+                        <PageNotFound />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </div>
+    )
+}
+
+
+export default App;
---
+++ React/lesson13/task2/src/Products.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Product from './Product.jsx'
+import { Link, Route } from "react-router-dom"
+
+function Products({ match }) {
+    return (
+        <div className="page__content">
+            <h1>Products</h1>
+            <ul className="navigation">
+                <li className="navigation__item">
+                    <Link to={`${match.url}/ball`}>Ball</Link>
+                </li>
+                <li className="navigation__item">
+                    <Link to={`${match.url}/book`}>Book</Link>
+                </li>
+            </ul>
+            <Route exact path="/products">
+                <span>Select a product please</span>
+            </Route>
+            <Route path="/products/:productId" component={Product} />
+        </div>
+    )
+}
+
+export default Products;
