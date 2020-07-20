51ac0e19f2cffc3e9d5e16a21ad85a62eddd48f7
Vitaly <nidastatachna@gmail.com>
2020-07-21 01:34:56 +0300

    component

--- React/lesson14/hw2/src/Clock.jsx
+++ React/lesson14/hw2/src/Clock.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
 const getTimeWithOffset = offset => {
     const currentTime = new Date();
@@ -6,39 +6,29 @@ const getTimeWithOffset = offset => {
     return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset))
 }
 
-class Clock extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            location: props.location,
-            offset: props.offset,
-            time: getTimeWithOffset(props.offset)
-        }
-    }
-    componentDidMount() {
-        this.interval = setInterval(() => {
-            this.setState({
-                time: getTimeWithOffset(this.state.offset)
-            })
+const Clock = (props) => {
+    const { offset, location } = props;
+    const [time, setTime] = useState(getTimeWithOffset(offset))
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(getTimeWithOffset(offset))
         }, 1000)
-    }
+        return () => {
+            clearInterval(interval)
+        }
+    }, [])
 
-    componentWillUnmount() {
-        clearInterval(this.interval)
-    }
-
-    render() {
-        return (
-            <div className="clock">
-                <div className="clock__location">
-                    {this.state.location}
-                </div>
-                <div className="clock__time">
-                    {this.state.time.toLocaleTimeString()}
-                </div>
+    return (
+        <div className="clock">
+            <div className="clock__location">
+                {location}
             </div>
-        )
-    }
+            <div className="clock__time">
+                {time.toLocaleTimeString()}
+            </div>
+        </div>
+    )
 }
 
 export default Clock;
