fed89679829386f435edce82a6c7e407da157d52
Vitaly <nidastatachna@gmail.com>
2020-07-28 11:55:37 +0300

    component

--- Redux/lesson3/hw1/src/index.js
+++ Redux/lesson3/hw1/src/index.js
@@ -3,13 +3,6 @@ import { setUser, removeUser } from './user.actions.js'
 import { setLanguage } from './language.actions.js'
 import store from './store.js'
 
-store.subscribe(() => console.log(store.getState()))
-
-store.dispatch(setLanguage('en'))
-store.dispatch(addProduct({ id: 1, name: 'coffe' }))
-store.dispatch(addProduct({ id: 2, name: 'milk' }))
-store.dispatch(removeProduct(1))
-store.dispatch(setUser({ id: 2212, name: 'Fima' }))
 
 
 
