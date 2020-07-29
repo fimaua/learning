7278b4abb7f89c41c09bf3a7b43ba21f1ee0f7a9
Vitaly <nidastatachna@gmail.com>
2020-07-29 23:23:42 +0300

    component

---
+++ Redux/lesson4/task2/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import Users from './users/Users.jsx';
+import store from './store.js';
+
+const App = () => {
+    return (
+        <Provider store={store}>
+            <Users />
+        </Provider>
+    )
+}
+export default App
