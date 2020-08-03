a1e1caa9b86ac58601a3b1b03a7997c88e4226bb
Vitaly <nidastatachna@gmail.com>
2020-08-03 12:11:12 +0300

    component

--- Redux/lesson4/hw1/src/users/users.reducer.js
+++ Redux/lesson4/hw1/src/users/users.reducer.js
@@ -1,42 +1,48 @@
 import { GO_NEXT, GO_PREV } from './users.actions.js'
 
 const initialState = {
-    usersList: [
-        {
-            id: 'id-0',
-            age: 21,
-            name: 'Bob',
-        },
-        {
-            id: 'id-1',
-            age: 17,
-            name: 'Tom',
-        },
-        {
-            id: 'id-2',
-            age: 18,
-            name: 'Tad',
-        },
-        {
-            id: 'id-3',
-            age: 45,
-            name: 'Sam',
-        },
-    ],
-    currentPage: 0,
+    users: {
+        usersList: [
+            {
+                id: 'id-0',
+                age: 21,
+                name: 'Bob',
+            },
+            {
+                id: 'id-1',
+                age: 17,
+                name: 'Tom',
+            },
+            {
+                id: 'id-2',
+                age: 18,
+                name: 'Tad',
+            },
+            {
+                id: 'id-3',
+                age: 45,
+                name: 'Sam',
+            },
+        ],
+        currentPage: 0,
+    }
 }
 
 export const usersReducer = (state = initialState, action) => {
     switch (action.type) {
         case GO_PREV:
             return {
-                ...state,
-                currentPage: state.currentPage - 1
+                users: {
+                    usersList: state.users.usersList,
+                    currentPage: state.users.currentPage - 1
+                }
             }
         case GO_NEXT: {
             return {
-                ...state,
-                currentPage: state.currentPage + 1
+                users: {
+                    usersList: state.users.usersList,
+                    currentPage: state.users.currentPage + 1
+                }
             }
         }
         default:
